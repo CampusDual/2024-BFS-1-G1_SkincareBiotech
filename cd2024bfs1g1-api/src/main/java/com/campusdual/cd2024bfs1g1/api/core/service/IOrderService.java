3ed6663a15a712c1c2d1e29a236265fef6a66748
McGestal <ricardovilarino@gmail.com>
2024-04-25 13:21:43 +0200

    Create OrderDao.xml, OrderDao.java, OrderService.java, IOrderService.java y OrderRestController.java

---
+++ cd2024bfs1g1-api/src/main/java/com/campusdual/cd2024bfs1g1/api/core/service/IOrderService.java
@@ -0,0 +1,18 @@
+package com.campusdual.cd2024bfs1g1.api.core.service;
+
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface IOrderService {
+
+    EntityResult orderQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
+
+    EntityResult orderInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+
+    EntityResult orderUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+
+    EntityResult orderDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+}
