4bb9e08b8b688fbff274b18bc236922a17ba6e8b
Pablo <pvalca99@gmail.com>
2024-04-25 13:25:31 +0200

    product view component and shared

---
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products-view/products-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-view',
+  templateUrl: './products-view.component.html',
+  styleUrls: ['./products-view.component.css']
+})
+export class ProductsViewComponent {
+
+}
--- cd2024bfs1g1-frontend/src/main/ngx/src/app/shared/shared.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/shared/shared.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { FilterComponent } from './components/filters/filters.component';
 import { HomeToolbarComponent } from './components/home-toolbar/home-toolbar.component';
+import { ProductsViewComponent } from '../main/products/products-view/products-view.component';
 
 @NgModule({
   imports: [
     OntimizeWebModule
+
   ],
   declarations: [
     FilterComponent,
-    HomeToolbarComponent
+    HomeToolbarComponent,
+    ProductsViewComponent
   ],
   exports: [
     CommonModule,
