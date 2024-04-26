efc5adc398d90107c4f53c6571b704559b605a9c
christianmp04 <christianmp04@gmail.com>
2024-04-26 13:25:37 +0200

    created insert product form

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { ProductsHomeComponent } from './products-home/products-home.component';
+import { ProductsNewComponent } from './products-new/products-new.component';
 
 
 @NgModule({
   declarations: [
-    ProductsHomeComponent
+    ProductsHomeComponent,
+    ProductsNewComponent
   ],
   imports: [
     CommonModule,
