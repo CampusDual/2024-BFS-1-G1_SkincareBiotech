e5c2bbd701d22c6de2e6c49c1c4f3e78bc9abc75
David Garcia Soto <David.gcia.soto@gmail.com>
2024-04-26 13:31:11 +0200

    fixed route

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/product-detail/product-detail.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { OntimizeService } from 'ontimize-web-ngx';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -13,15 +14,17 @@ export class ProductDetailComponent implements OnInit {
   product: any = {};
   constructor(
     protected injector: Injector,
-    protected sanitizer: DomSanitizer
+    protected sanitizer: DomSanitizer,
+    private route: ActivatedRoute
   ) {
     this.service = this.injector.get(OntimizeService)
   }
 
   ngOnInit(): void {
+    let id = parseInt(this.route.snapshot.paramMap.get('prod_id'))
     const conf = this.service.getDefaultServiceConfiguration('products');
     this.service.configureService(conf);
-    this.service.query({ "PRO_ID": 1 }, ["PRO_NAME", "PRO_DESCRIPTION", "PRO_PRICE", "PRO_IMAGE"], "product")
+    this.service.query({ "PRO_ID": id }, ["PRO_NAME", "PRO_DESCRIPTION", "PRO_PRICE", "PRO_IMAGE"], "product")
       .subscribe((data) => {
         console.log(data);
         this.product = data.data[0];
