b33defcbf54c1d9bae1a35f53adecb181f6907cb
David Garcia Soto <David.gcia.soto@gmail.com>
2024-04-26 12:51:00 +0200

    product displays correctly

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/product-detail/product-detail.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/product-detail/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { OntimizeService } from 'ontimize-web-ngx';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-detail',
@@ -8,20 +9,27 @@ import { OntimizeService } from 'ontimize-web-ngx';
 })
 export class ProductDetailComponent implements OnInit {
 
-  service : OntimizeService;
-  product :any = {};
-  constructor(protected injector: Injector) {
-    this.service = this.injector.get(OntimizeService);
+  service: OntimizeService;
+  product: any = {};
+  constructor(
+    protected injector: Injector,
+    protected sanitizer: DomSanitizer
+  ) {
+    this.service = this.injector.get(OntimizeService)
   }
 
   ngOnInit(): void {
     const conf = this.service.getDefaultServiceConfiguration('products');
-  this.service.configureService(conf);
-    this.service.query({ "PRO_ID": 1 }, ["PRO_NAME", "PRO_PRICE"], "product")
+    this.service.configureService(conf);
+    this.service.query({ "PRO_ID": 1 }, ["PRO_NAME", "PRO_DESCRIPTION", "PRO_PRICE", "PRO_IMAGE"], "product")
       .subscribe((data) => {
         console.log(data);
-        this.product=data.data[0];
+        this.product = data.data[0];
       })
+  }
+
+  public getImageSrc(base64: any): any {
+    return base64 ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/*;base64,' + base64) : './assets/images/no-image.png';
   }
 
 }
