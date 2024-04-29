22b13e82d86bde4c77c48caf33c0ca1525b74c23
acasella03 <114586433+acasella03@users.noreply.github.com>
2024-04-29 15:13:18 +0200

    Created orders details

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/orders/orders-routing.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/orders/orders-routing.module.ts
@@ -1,11 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrdersHomeComponent } from './orders-home/orders-home.component';
+import { OrdersDetailComponent } from './orders-detail/orders-detail.component';
 
 const routes: Routes = [
   {
   path:'',
   component: OrdersHomeComponent
+  },
+  {
+    path:':ORD_ID',
+    component: OrdersDetailComponent
   }
 ];
 
--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/orders/orders.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/orders/orders.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersHomeComponent } from './orders-home/orders-home.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { OrdersDetailComponent } from './orders-detail/orders-detail.component';
 
 
 @NgModule({
   declarations: [
-    OrdersHomeComponent
+    OrdersHomeComponent,
+    OrdersDetailComponent
   ],
   imports: [
     OntimizeWebModule,
