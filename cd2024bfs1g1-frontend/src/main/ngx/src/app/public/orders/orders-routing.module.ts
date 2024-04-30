910a2b2756e76d245c7996a7c36269ce4730097e
BryanGrana <bryangranamartinez@gmail.com>
2024-04-30 13:40:21 +0200

    Created public orders components

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/orders/orders-routing.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/orders/orders-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewOrderComponent } from './new-order/new-order.component';
+import { NewOrderDetailsComponent } from './new-order-details/new-order-details.component';
 
 const routes: Routes = [{
-  path: '',
+  path: ':PRO_ID',
   component: NewOrderComponent
+}, {
+  path: 'details/:ORD_ID',
+  component: NewOrderDetailsComponent
 }];
 
 @NgModule({
--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/orders/orders.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/orders/orders.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { NewOrderComponent } from './new-order/new-order.component';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+import { NewOrderDetailsComponent } from './new-order-details/new-order-details.component';
 
 
 @NgModule({
   declarations: [
-    NewOrderComponent
+    NewOrderComponent,
+    NewOrderDetailsComponent
   ],
   imports: [
     CommonModule,
