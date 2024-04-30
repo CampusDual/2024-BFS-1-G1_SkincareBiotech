2a20a359c16731f9d08bd1f102f64b1a043bc151
Lain <diegolain.personal@gmail.com>
2024-04-30 11:08:45 +0200

    added product detail view - no checkbox place yet

---
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products-detail/products-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-detail',
+  templateUrl: './products-detail.component.html',
+  styleUrls: ['./products-detail.component.css']
+})
+export class ProductsDetailComponent {
+
+
+}
