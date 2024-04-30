1c6edd80e0949ed2f0e2066da99604b6188492ec
Tania Vazquez Pose <tvaquezpose@gmail.com>
2024-04-30 14:13:11 +0200

    Corrected script V5

--- cd2024bfs1g1-model/src/main/resources/db/migration/V5__create_seller_user_rol.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V5__create_seller_user_rol.sql
@@ -4,10 +4,6 @@ VALUES (3,'seller',
 	'{ "menu": [{ "attr": "admin", "visible": false, "enabled": false }]}',
 	'This is the seller role');
 
-UPDATE public.usr_role
-SET rol_json_client_permission = '{ "menu": [{ "attr": "admin", "visible": false, "enabled": false },{"attr": "products", "visible": false, "enabled": false}] }'
-WHERE rol_id = 2;
-
 INSERT INTO public.usr_user (usr_id,usr_login,usr_name,usr_surname,usr_email,usr_password,usr_notes)
 	VALUES (3,'seller','Seller Name','Seller Surname','selleruser@mail.com','$2a$12$97NEAHKTE4wWjSndPjzDd.idjbdorhdUXFwu9XMZj1RNcCXbwApju','This is the seller user');
 
