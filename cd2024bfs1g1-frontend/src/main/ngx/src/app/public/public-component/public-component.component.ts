4652c292c11df5a264f78a280d2bbfee12ff4694
EduFergo <efergo93@gmail.com>
2024-04-30 11:41:36 +0200

    Created NavBar in public module

---
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-component/public-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-component',
+  templateUrl: './public-component.component.html',
+  styleUrls: ['./public-component.component.scss']
+})
+export class PublicComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-routing.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponentComponent } from './public-component/public-component.component';
 
 const routes: Routes = [
   {
+    component: PublicComponentComponent,
     path: '',
     children: [
       { path: '', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
