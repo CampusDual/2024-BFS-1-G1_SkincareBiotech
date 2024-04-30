cc5cd54a8c8b3ad2337aae57c8ee0d286cb5f446
Tania Vazquez Pose <tvaquezpose@gmail.com>
2024-04-30 13:18:36 +0200

    Updated user role

--- cd2024bfs1g1-model/src/main/resources/db/migration/V5__create_seller_user_rol.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V5__create_seller_user_rol.sql
@@ -13,3 +13,7 @@ INSERT INTO public.usr_user (usr_id,usr_login,usr_name,usr_surname,usr_email,usr
 
 INSERT INTO public.usr_user_role (usr_id,rol_id)
 	VALUES (3,3);
+
+UPDATE public.usr_role
+	SET rol_json_client_permission='{ "menu": [{ "attr": "admin", "visible": false, "enabled": false },{"attr": "products", "visible": false, "enabled": false}, {"attr": "orders", "visible": false, "enabled": false}] }'
+	WHERE rol_id=2;
