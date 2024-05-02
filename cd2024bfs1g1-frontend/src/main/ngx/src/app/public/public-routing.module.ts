d59709b8a7b64144a462d417d435551bce6130a6
Lain <diegolain.personal@gmail.com>
2024-05-02 13:44:43 +0200

    fixed merge sb_16 with develop

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-routing.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-routing.module.ts
@@ -3,8 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { PublicComponentComponent } from './public-component/public-component.component';
 
 const routes: Routes = [
+  {
+    component: PublicComponentComponent,
+    path: '',
+    children: [
+      { path: '', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+    ]
+  }, 
   { path: 'order', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
-  { path: '', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: '', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+
 ];
 
 @NgModule({
