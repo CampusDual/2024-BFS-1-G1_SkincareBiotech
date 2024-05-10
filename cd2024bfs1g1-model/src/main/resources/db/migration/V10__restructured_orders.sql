973f338970036e2d49b15da4aaadc617cdf02df8
EduFergo <efergo93@gmail.com>
2024-05-10 15:05:53 +0200

    ord_detail renamed ord_lines, deleted price on order, added on lines

--- cd2024bfs1g1-model/src/main/resources/db/migration/V10__restructured_orders.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V10__restructured_orders.sql
@@ -1,13 +1,15 @@
 ALTER TABLE public.orders DROP CONSTRAINT orders_products_fk;
 ALTER TABLE public.orders DROP COLUMN pro_id;
+ALTER TABLE public.orders DROP COLUMN ord_price;
 
-CREATE TABLE public.ord_detail (
-	od_id serial NOT NULL,
+CREATE TABLE public.ord_lines (
+	ol_id serial NOT NULL,
 	pro_id integer NOT NULL,
-	od_units integer NOT NULL,
-	CONSTRAINT ord_detail_pk PRIMARY KEY (od_id),
-	CONSTRAINT ord_detail_products_fk FOREIGN KEY (pro_id) REFERENCES public.products(pro_id)
+	ol_price integer NOT NULL,
+	ol_units integer NOT NULL,
+	CONSTRAINT ord_lines_pk PRIMARY KEY (ol_id),
+	CONSTRAINT ord_lines_products_fk FOREIGN KEY (pro_id) REFERENCES public.products(pro_id)
 );
 
-ALTER TABLE public.orders ADD od_id integer NOT NULL;
-ALTER TABLE public.orders ADD CONSTRAINT orders_ord_detail_fk FOREIGN KEY (od_id) REFERENCES public.ord_detail(od_id);
+ALTER TABLE public.orders ADD ol_id integer NOT NULL;
+ALTER TABLE public.orders ADD CONSTRAINT orders_ord_lines_fk FOREIGN KEY (ol_id) REFERENCES public.ord_lines(ol_id);
