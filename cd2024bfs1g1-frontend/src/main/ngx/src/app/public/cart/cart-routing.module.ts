8a53ac5679f9ef906474dc465a37c59bf1b81d31
Sergio Méndez Losada <sergio.mendez@campusdual.com>
2024-05-10 11:00:52 +0200

    Added cart-item component

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/cart/cart-routing.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/cart/cart-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartViewComponent } from './cart-view/cart-view.component';
+import { CartItemComponent } from './cart-view/cart-item/cart-item.component';
 
 const routes: Routes = [
-  { path: 'cart', component: CartViewComponent}
+  { path: 'cart', component: CartViewComponent},
+  { path: 'view', component: CartItemComponent}
 ];
 
 @NgModule({
--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/cart/cart.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/cart/cart.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
-
+import { CartItemComponent } from './cart-view/cart-item/cart-item.component';
 import { CartRoutingModule } from './cart-routing.module';
 import { CartViewComponent } from './cart-view/cart-view.component';
 
 
 @NgModule({
   declarations: [
-    CartViewComponent
+    CartViewComponent,
+    CartItemComponent
   ],
   imports: [
     CommonModule,
