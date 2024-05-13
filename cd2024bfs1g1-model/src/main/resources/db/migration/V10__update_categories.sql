7a176ebf216145e979120a94159111cece25f315
Lain <diegolain.personal@gmail.com>
2024-05-13 13:08:57 +0200

    adding brand to product-new and product-detail

---
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V10__update_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.categories (cat_name)
+VALUES ('Default');
+UPDATE products
+SET cat_id = (
+    SELECT cat_id
+    FROM categories
+    WHERE cat_name = 'Default'
+)
+WHERE cat_id is null;
--- cd2024bfs1g1-model/src/main/resources/db/migration/V11__create_brands_table.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V11__create_brands_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE public.brands (bra_id serial NOT NULL,
                                 CONSTRAINT brands_pk PRIMARY KEY (bra_id));
 ALTER TABLE public.products ADD bra_id int NULL;
 ALTER TABLE public.products ADD CONSTRAINT products_brands_fk FOREIGN KEY (bra_id) REFERENCES public.brands(bra_id);
-INSERT INTO public.brands (bra_id, bra_name) VALUES(1, 'Sin marca');
+INSERT INTO public.brands (bra_id, bra_name) VALUES(1, 'Default');
 UPDATE public.products SET bra_id = 1 WHERE bra_id IS NULL;
 ALTER TABLE public.products ALTER COLUMN bra_id SET NOT NULL;
