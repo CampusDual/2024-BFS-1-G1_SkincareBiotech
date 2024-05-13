5b829278e68d54fec580048ee2fe4483b275bff3
Anxo Varela <varelarodriguezanxo@gmail.com>
2024-05-13 10:31:52 +0200

    Fix public permission json and call CustomPermissionService.

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-component/public-component.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-component/public-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomPermissionsService } from 'src/app/shared/services/custom-permissions.service';
 
 @Component({
   selector: 'app-public-component',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private permissionService: CustomPermissionsService,
+  ) { }
 
   ngOnInit() {
+    this.permissionService.loadPermissions().subscribe();
   }
 
 }
