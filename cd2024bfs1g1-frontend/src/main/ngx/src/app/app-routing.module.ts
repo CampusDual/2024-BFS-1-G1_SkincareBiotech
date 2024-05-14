690a2d044662779e66dce3a0ae87b66863ec0262
Carlos A. S <as.carlos.dev@gmail.com>
2024-05-14 12:01:37 +0200

    Routing Fixed

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/app-routing.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { AuthGuardService } from 'ontimize-web-ngx';
 
 
-export const routes: Routes = [
-  { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
+export const routes: Routes = [   
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'main', canActivate: [AuthGuardService], loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+  { path: '', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) },
   { path: '**', redirectTo: '' }
 ];
 
