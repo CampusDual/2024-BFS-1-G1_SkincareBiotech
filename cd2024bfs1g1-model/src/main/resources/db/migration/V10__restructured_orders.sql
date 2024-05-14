4e7b61822eab4db7f9624c4aa7f42bb48f7723f1
EduFergo <efergo93@gmail.com>
2024-05-14 09:54:55 +0200

    fixed orders db

--- cd2024bfs1g1-model/src/main/resources/db/migration/V10__restructured_orders.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V10__restructured_orders.sql
@@ -1,15 +1,17 @@
 ALTER TABLE public.orders DROP CONSTRAINT orders_products_fk;
 ALTER TABLE public.orders DROP COLUMN pro_id;
 ALTER TABLE public.orders DROP COLUMN ord_price;
+ALTER TABLE public.orders ADD ord_date timestamp DEFAULT now() NOT NULL;
+
 
 CREATE TABLE public.ord_lines (
 	ol_id serial NOT NULL,
+	ord_id integer NOT NULL,
 	pro_id integer NOT NULL,
 	ol_price integer NOT NULL,
 	ol_units integer NOT NULL,
 	CONSTRAINT ord_lines_pk PRIMARY KEY (ol_id),
+	CONSTRAINT ord_lines_orders_fk FOREIGN KEY (ord_id) REFERENCES public.orders(ord_id),
 	CONSTRAINT ord_lines_products_fk FOREIGN KEY (pro_id) REFERENCES public.products(pro_id)
 );
 
-ALTER TABLE public.orders ADD ol_id integer NOT NULL;
-ALTER TABLE public.orders ADD CONSTRAINT orders_ord_lines_fk FOREIGN KEY (ol_id) REFERENCES public.ord_lines(ol_id);
