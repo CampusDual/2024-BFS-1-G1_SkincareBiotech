587c6258f0b9f1f242e13f9ac4dc5c03485ba5cb
Lain <diegolain.personal@gmail.com>
2024-05-15 11:13:29 +0200

    filtering price by range fix

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-products/products-view/products-view.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/public-products/products-view/products-view.component.ts
@@ -10,10 +10,6 @@ import { FilterExpressionUtils, Expression, OntimizeService, ORealInputComponent
 
 export class ProductsViewComponent implements OnInit {
 
-  // @ViewChild ("maxPriceInput")
-  // maxPriceInput : ORealInputComponent
-  // @ViewChild ("minPriceInput")
-  // minPriceInput : ORealInputComponent
 
   service: OntimizeService;
 
@@ -28,12 +24,11 @@ export class ProductsViewComponent implements OnInit {
     protected sanitizer: DomSanitizer
   ) {
     this.service = this.injector.get(OntimizeService)
-   }
-  
+  }
+
   ngOnInit(): void {
 
-      // this.maxPriceInput = this.getPrice(false) ;
-      // this.precioMinimo = this.getPrice(true) as number;
+
 
   }
 
@@ -50,27 +45,22 @@ export class ProductsViewComponent implements OnInit {
         if (fil.attr === 'CAT_ID') {
           filters.push(FilterExpressionUtils.buildExpressionEquals(fil.attr, fil.value));
         }
-        if(fil.attr ==='BRA_ID'){
+        if (fil.attr === 'BRA_ID') {
           filters.push(FilterExpressionUtils.buildExpressionEquals(fil.attr, fil.value));
         }
-        if(fil.attr == 'PRO_MAX_PRICE'){
-           this.maxPrice = FilterExpressionUtils.buildExpressionLessEqual("PRO_PRICE", fil.value);          
-           this.maxSale = FilterExpressionUtils.buildExpressionLessEqual("PRO_SALE", fil.value);
-          filters.push(FilterExpressionUtils.buildComplexExpression(this.maxPrice, this.maxSale, FilterExpressionUtils.OP_OR));
+        if (fil.attr == 'PRO_MAX_PRICE') {
+          let value: number = Number(fil.value);
+          filters.push(FilterExpressionUtils.buildExpressionLessEqual("PRO_REAL_PRICE", value));
         }
-        if(fil.attr == 'PRO_MIN_PRICE'){
-          this.minPrice = FilterExpressionUtils.buildExpressionMoreEqual("PRO_PRICE", fil.value);
-          this.minSale = FilterExpressionUtils.buildExpressionMoreEqual("PRO_SALE", fil.value);
-          filters.push(FilterExpressionUtils.buildComplexExpression(this.minPrice, this.minSale, FilterExpressionUtils.OP_OR));
-        }  
-      
-        
+        if (fil.attr == 'PRO_MIN_PRICE') {
+          let value: number = Number(fil.value);
+          filters.push(FilterExpressionUtils.buildExpressionMoreEqual("PRO_REAL_PRICE", value));
+        }
       }
     });
 
     // Build complex expression
     if (filters.length > 0) {
-
       return filters.reduce((exp1, exp2) => FilterExpressionUtils.buildComplexExpression(exp1, exp2, FilterExpressionUtils.OP_AND));
     } else {
       return null;
@@ -78,30 +68,5 @@ export class ProductsViewComponent implements OnInit {
   }
 
 
-  // getPrice(orderColumn: boolean): any{
-  //   const conf = this.service.getDefaultServiceConfiguration('products');
-  //   this.service.configureService(conf);
-  //   const columns = [
-  //     "PRO_PRICE",
-  //     "PRO_SALE"
-  //   ];
-  //   const filter = { "PRO_ENABLED": true };
-  //   const order = [{ "columnName": "PRO_PRICE", "ascendent": orderColumn }];
-  //   this.service.advancedQuery(filter, columns, "product", null, 0, 1, order)
-  //     .subscribe((data) => {
-  //       if (data.data.length > 0) {
 
-  //         if(data.data[0].PRO_SALE !== null){
-  //           this.price = data.data[0].PRO_SALE;
-            
-  //         }else{
-  //           this.price = data.data[0].PRO_PRICE;
-  //         }
-  //       }
-  //     }) 
-
-  //     return this.price;
-  // }
-
-  
 }
