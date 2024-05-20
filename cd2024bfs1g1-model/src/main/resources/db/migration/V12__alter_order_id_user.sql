9888c8a21a0d069d68dd6bf9afe6b7608e603f92
XoanOuteiro <xoanouteirojorge@gmail.com>
2024-05-20 11:24:28 +0200

    fixed permissions for users in brands and orders

--- cd2024bfs1g1-model/src/main/resources/db/migration/V12__alter_order_id_user.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V12__alter_order_id_user.sql
@@ -3,7 +3,7 @@ ALTER TABLE public.orders ADD ord_client_id int4 NOT NULL;
 ALTER TABLE public.orders ADD CONSTRAINT orders_usr_user_fk FOREIGN KEY (ord_client_id) REFERENCES public.usr_user(usr_id);
 
 UPDATE public.usr_role
-	SET rol_json_client_permission='{ "menu": [ { "attr": "products", "visible": false, "enabled": false }, { "attr": "orders", "visible": false, "enabled": false }, { "attr": "categories", "visible": false, "enabled": false }, { "attr": "home", "visible": false, "enabled": false }, { "attr": "login", "visible": false, "enabled": false }, { "attr": "logout", "visible": false, "enabled": false } ] }'
+	SET rol_json_client_permission='{ "menu": [ { "attr": "products", "visible": false, "enabled": false }, { "attr": "orders", "visible": false, "enabled": false },{ "attr": "brands", "visible": false, "enabled": false }, { "attr": "categories", "visible": false, "enabled": false }, { "attr": "home", "visible": false, "enabled": false }, { "attr": "login", "visible": false, "enabled": false }, { "attr": "logout", "visible": false, "enabled": false } ] }'
 	WHERE rol_name= 'user';
 UPDATE public.usr_role
     SET rol_json_client_permission='{ "menu": [ { "attr": "productsPublic", "visible": false, "enabled": false }, { "attr": "featuredPublic", "visible": false, "enabled": false }, { "attr": "history", "visible": false, "enabled": false }, { "attr": "login", "visible": false, "enabled": false } ] }'
