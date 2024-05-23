813ccb236b6bc046ef66167630d2f54698baa8d6
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2024-05-23 14:21:29 +0200

    Created branch sb_99

---
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/categories/categories-detail/categories-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories-detail',
+  templateUrl: './categories-detail.component.html',
+  styleUrls: ['./categories-detail.component.css']
+})
+export class CategoriesDetailComponent {
+
+}
