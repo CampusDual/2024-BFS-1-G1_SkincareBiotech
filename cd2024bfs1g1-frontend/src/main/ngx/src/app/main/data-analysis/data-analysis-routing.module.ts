3d8490b08d29113ac2107163092a732db8a379d5
EduFergo <efergo93@gmail.com>
2024-05-24 15:12:22 +0200

    created billed-age component

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/data-analysis-routing.module.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/data-analysis-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataAnalysisHomeComponent } from './data-analysis-home/data-analysis-home.component';
+import { BilledAgeComponent } from './billed-age/billed-age.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DataAnalysisHomeComponent
+  },
+  {
+    path: 'billed-age',
+    component: BilledAgeComponent
   }
 ];
 
