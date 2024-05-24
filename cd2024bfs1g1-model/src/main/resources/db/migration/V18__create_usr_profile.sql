4bbe6b1569e0713574d5dd2f1f8b955d0e546d14
Susana Roca <sroca754@gmail.com>
2024-05-24 14:53:14 +0200

    modified v18 and create gra_billed_age table

--- cd2024bfs1g1-model/src/main/resources/db/migration/V18__create_usr_profile.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V18__create_usr_profile.sql
@@ -1,7 +1,13 @@
 CREATE TABLE public.usr_profile (
-	upr_id serial NOT NULL,
-	usr_id integer NOT NULL,
+	usr_id int4 NOT NULL,
 	upr_birthdate date NOT NULL,
-	CONSTRAINT usr_profile_pk PRIMARY KEY (upr_id),
+	CONSTRAINT usr_profile_pk PRIMARY KEY (usr_id),
 	CONSTRAINT usr_profile_usr_user_fk FOREIGN KEY (usr_id) REFERENCES public.usr_user(usr_id)
+);
+
+CREATE TABLE public.gra_billed_age (
+	gba_id serial NOT NULL,
+	gba_min_age integer NOT NULL,
+	gba_max_age integer NOT NULL,
+	CONSTRAINT gra_billed_age_pk PRIMARY KEY (gba_id)
 );
