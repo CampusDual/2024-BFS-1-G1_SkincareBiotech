43db8dda0d9d8227ccc55ea1ca54e6fe3e46df94
XoanOuteiro <xoanouteirojorge@gmail.com>
2024-05-28 13:25:19 +0200

    fixed permissions error where admin had cluttered lateral navbar

--- cd2024bfs1g1-model/src/main/resources/db/migration/V18__alter_graph_permission.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V18__alter_graph_permission.sql
@@ -12,3 +12,12 @@ UPDATE public.usr_role
 UPDATE public.usr_role
 	SET rol_json_client_permission='{ "menu": [ { "attr": "productsPublic", "visible": false, "enabled": false }, { "attr": "featuredPublic", "visible": false, "enabled": false }, { "attr": "history", "visible": false, "enabled": false }, { "attr": "login", "visible": false, "enabled": false }, { "attr": "cart", "visible": false, "enabled": false },{"attr": "sells-by-category", "visible": false, "enabled": false } ] }'
 	WHERE rol_id=3;
+update public.usr_role
+	set rol_json_client_permission = '{ "menu": [ { "attr": "products", "visible": false, "enabled": false },
+	{ "attr": "orders", "visible": false, "enabled": false },{ "attr": "brands", "visible": false, "enabled": false },
+	{ "attr": "categories", "visible": false, "enabled": false }, { "attr": "home", "visible": false, "enabled": false },
+	{ "attr": "login", "visible": false, "enabled": false }, { "attr": "logout", "visible": false, "enabled": false },
+	{ "attr": "data-analysis", "visible": false, "enabled": false },
+	{ "attr": "history", "visible": false, "enabled": false },
+	{ "attr": "productsPublic", "visible": false, "enabled": false }] }'
+	where rol_id=1;
