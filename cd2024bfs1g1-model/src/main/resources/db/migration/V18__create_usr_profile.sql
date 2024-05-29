694bda4f2718742eb4ddfdc3346238421d38cfac
EduFergo <efergo93@gmail.com>
2024-05-29 08:28:17 +0200

    fixed merge bugs

--- cd2024bfs1g1-model/src/main/resources/db/migration/V18__create_usr_profile.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V18__create_usr_profile.sql
@@ -22,7 +22,8 @@ UPDATE public.usr_role
 UPDATE public.usr_role
     SET rol_json_client_permission='{ "menu": [ { "attr": "productsPublic", "visible": false, "enabled": false },
     { "attr": "featuredPublic", "visible": false, "enabled": false }, { "attr": "history", "visible": false, "enabled": false },
-    { "attr": "login", "visible": false, "enabled": false }, { "attr": "billed-age", "visible": false, "enabled": false } ] }'
+    { "attr": "login", "visible": false, "enabled": false }, { "attr": "billed-age", "visible": false, "enabled": false },
+    { "attr": "cart", "visible": false, "enabled": false } ] }'
     WHERE rol_name= 'seller';
 UPDATE public.usr_role
 	SET rol_json_client_permission='{ "menu": [ { "attr": "productsPublic", "visible": false, "enabled": false },
