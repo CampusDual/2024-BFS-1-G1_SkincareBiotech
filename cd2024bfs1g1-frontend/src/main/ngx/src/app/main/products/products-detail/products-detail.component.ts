b98c87529e93287d73065843f736177ed8ae3b95
iagopombo <iagopombo@gmail.com>
2024-05-31 14:33:21 +0200

    Update product-detail with price & commissions

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products-detail/products-detail.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products-detail/products-detail.component.ts
@@ -1,13 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Injector, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { OCurrencyInputComponent, OSlideToggleComponent } from 'ontimize-web-ngx';
+import { OCurrencyInputComponent, OSlideToggleComponent, OntimizeService } from 'ontimize-web-ngx';
 
 @Component({
   selector: 'app-products-detail',
   templateUrl: './products-detail.component.html',
   styleUrls: ['./products-detail.component.css']
 })
-export class ProductsDetailComponent {
+export class ProductsDetailComponent implements OnInit{
 
   @ViewChild ("proSaleToggle")
   proSaleToggle : OSlideToggleComponent;
@@ -15,9 +15,35 @@ export class ProductsDetailComponent {
   @ViewChild ("proSaleCurrency")
   proSaleCurrency : OCurrencyInputComponent;
 
+  @ViewChild("proPriceCurrency")
+  proPriceCurrency: OCurrencyInputComponent;
+  
+  service: OntimizeService;
+
+  commissionPlataform: number;
+  commissionRedSys: number;
+
+  public priceUser: number;
+
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    protected injector: Injector
+  ) { 
+    this.service = this.injector.get(OntimizeService);
+    }
+
+  ngOnInit(){
+    const conf = this.service.getDefaultServiceConfiguration('commissions');
+      this.service.configureService(conf);
+      this.service.query({}, ["COM_NAME","COM_VALUE"], "commission")
+        .subscribe((data) => {
+          if (data.data.length > 0) {
+            this.commissionRedSys = data.data.find((element) => (element.COM_NAME === "Redsys_commissions")).COM_VALUE;            
+            this.commissionPlataform = data.data.find((element) => (element.COM_NAME === "Plataform_commissions")).COM_VALUE;
+          }
+        })
+      this.priceUser = 0;
+  }
 
   onUpdate(success: boolean) {
     if (success) {
@@ -41,6 +67,16 @@ export class ProductsDetailComponent {
     }
   
     this.proSaleCurrency.setEnabled(this.proSaleToggle.getValue());
+  }
+
+  onInputChanged(event){
+    if (!this.proSaleToggle.isChecked()) {
+      this.priceUser = (this.proPriceCurrency.getValue() / (1 - (this.commissionPlataform / 100))) / (1 - (this.commissionRedSys / 100));
+      this.proPriceCurrency.readOnly = false;
+    } else {
+      this.priceUser = (this.proSaleCurrency.getValue() / (1 - (this.commissionPlataform / 100))) / (1 - (this.commissionRedSys / 100));
+      this.proPriceCurrency.readOnly = true;
+    }
   }
 
 }
