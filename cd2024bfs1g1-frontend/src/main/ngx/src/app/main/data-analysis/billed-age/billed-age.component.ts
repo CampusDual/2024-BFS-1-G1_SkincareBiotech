2d29e658c095a7c3dc9b8d764cd743025cf2c540
Susana Roca <sroca754@gmail.com>
2024-05-31 14:38:08 +0200

    changed color

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/billed-age/billed-age.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/billed-age/billed-age.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { PieChartConfiguration } from 'ontimize-web-ngx-charts';
-import { MultiBarHorizontalChartConfiguration } from 'ontimize-web-ngx-charts';
-import { MultiBarChartConfiguration } from 'ontimize-web-ngx-charts';
 
 @Component({
   selector: 'app-billed-age',
@@ -10,24 +8,18 @@ import { MultiBarChartConfiguration } from 'ontimize-web-ngx-charts';
 })
 export class BilledAgeComponent {
 
-  data: Array<Object>;
-  protected serviceResponse: string;
+  chartParameters: PieChartConfiguration;
 
-  chartParameters:MultiBarChartConfiguration;
-  chartParameters2: PieChartConfiguration;
-
-  // colorScheme = {
-  //   domain: ['#7ec0ee', '#40e0d0', '#003366', '#4682b4']
-  // };
+  colorScheme = {
+    domain: ['#31d4f8', '#2aaecb', '#1f6e9a', '#154865', '#0499ec', '#03649b', '#03649b']
+  };
 
   constructor() {
-    this.chartParameters2 = new PieChartConfiguration();
-    this.chartParameters2.labelType = 'value';
-    this.chartParameters2.legend.margin.top = 5;
-    this.chartParameters2.legend.margin.bottom = 5;
-    this.chartParameters2.legendPosition = 'right';
-    this.chartParameters = new MultiBarChartConfiguration();
-    this
+    this.chartParameters = new PieChartConfiguration();
+    this.chartParameters.labelType = 'value';
+    this.chartParameters.legend.margin.top = 5;
+    this.chartParameters.legend.margin.bottom = 5;
+    this.chartParameters.legendPosition = 'right';
   }
 
 }
