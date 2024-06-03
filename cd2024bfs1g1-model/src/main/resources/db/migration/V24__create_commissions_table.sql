f7cfb2456eb22e9cf17a562c4ec518118da82a3d
iagopombo <iagopombo@gmail.com>
2024-06-03 09:42:44 +0200

    Update script 24

--- cd2024bfs1g1-model/src/main/resources/db/migration/V24__create_commissions_table.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V24__create_commissions_table.sql
@@ -18,10 +18,28 @@ AS WITH cr AS (
            FROM commissions c
           WHERE c.com_name::text = 'Plataform_commissions'::text
         )
- SELECT p.pro_id,
-    ROUND((p.pro_price / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100)),2) AS user_final_price,
-    ROUND((p.pro_sale / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100)),2) AS user_final_price_sale,
-    ROUND(COALESCE((p.pro_sale / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100)), (p.pro_price / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100))),2) as user_final_price_checked_sale
-   FROM products p,
-    cr,
-    cp;
+SELECT p.pro_id,
+   ROUND((p.pro_price / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100)),2) AS user_final_price,
+   ROUND((p.pro_sale / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100)),2) AS user_final_price_sale,
+   ROUND(COALESCE((p.pro_sale / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100)), (p.pro_price / (1 - (cp.com_pla::numeric/100))) / (1-(cr.com_redsys::numeric/100))),2) as user_final_price_checked_sale
+  FROM products p,
+   cr,
+   cp;
+
+UPDATE public.usr_role
+    SET rol_json_client_permission='{ "menu": [ { "attr": "productsPublic", "visible": false, "enabled": false }, { "attr": "featuredPublic", "visible": false, "enabled": false }, { "attr": "history", "visible": false, "enabled": false }, { "attr": "login", "visible": false, "enabled": false }, { "attr": "cart", "visible": false, "enabled": false }, { "attr": "sellers", "visible": false, "enabled": false }, { "attr": "commissions", "visible": false, "enabled": false } ] }'
+    WHERE rol_name= 'seller';
+
+UPDATE public.usr_role
+    SET rol_json_client_permission='{ "menu": [ { "attr": "products", "visible": false, "enabled": false },
+                                       { "attr": "orders", "visible": false, "enabled": false },
+                                       { "attr": "brands", "visible": false, "enabled": false },
+                                       { "attr": "categories", "visible": false, "enabled": false },
+                                       { "attr": "home", "visible": false, "enabled": false },
+                                       { "attr": "login", "visible": false, "enabled": false },
+                                       { "attr": "logout", "visible": false, "enabled": false },
+                                       {"attr": "data-analysis", "visible": false, "enabled": false },
+                                       { "attr": "sellers", "visible": false, "enabled": false },
+                                       { "attr": "billed-age", "visible": false, "enabled": false },
+                                       { "attr": "commissions", "visible": false, "enabled": false }]}'
+    WHERE rol_name= 'user';
