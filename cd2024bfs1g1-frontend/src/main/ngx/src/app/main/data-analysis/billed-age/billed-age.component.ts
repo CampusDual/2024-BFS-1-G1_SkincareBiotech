c7aa91c3f27f49934847b1d158a10008a5216814
EduFergo <efergo93@gmail.com>
2024-06-07 13:26:44 +0200

    message added to billedbyage

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/billed-age/billed-age.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/billed-age/billed-age.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { PieChartConfiguration } from 'ontimize-web-ngx-charts';
+import { OntimizeService } from 'ontimize-web-ngx';
+
 
 @Component({
   selector: 'app-billed-age',
@@ -8,7 +10,16 @@ import { PieChartConfiguration } from 'ontimize-web-ngx-charts';
 })
 export class BilledAgeComponent {
 
+  data: any[];
   chartParameters: PieChartConfiguration;
+  service: OntimizeService;
+
+  minAge: number;
+  maxAge: number;
+  maxBilledByAge: number;
+  totalBilled: number;
+  percentage: number;
+  isData: boolean;
 
   colorScheme = {
     domain: ['#31d4f8', '#2aaecb', '#1f6e9a', '#154865', '#0499ec', '#03649b', '#03649b']
@@ -20,6 +31,41 @@ export class BilledAgeComponent {
     this.chartParameters.legend.margin.top = 5;
     this.chartParameters.legend.margin.bottom = 5;
     this.chartParameters.legendPosition = 'right';
+  }
+
+  loadChart(event: any) {
+    this.minAge=0;
+    this.maxAge=0;
+    this.maxBilledByAge=0;
+    this.totalBilled=0;
+    this.isData = false;
+    const groupedData = event.reduce((acc, item) => {
+      if (!acc[item.AGE_RANGE]) {
+        acc[item.AGE_RANGE] = 0;
+      }
+      acc[item.AGE_RANGE] += item.TOTAL;
+      this.isData=true;
+      return acc;
+    }, {});
+
+    this.data = Object.keys(groupedData).map(key => {
+      return {
+        name: key,
+        value: groupedData[key]
+      };
+    });
+    for (let ageRange in event) {
+      console.log(event[ageRange])
+      if (event[ageRange].TOTAL > this.maxBilledByAge) {
+        this.maxBilledByAge = event[ageRange].TOTAL;
+        this.minAge = event[ageRange].GBA_MIN_AGE;
+        this.maxAge = event[ageRange].GBA_MAX_AGE;
+      }
+      this.totalBilled += event[ageRange].TOTAL;
+    }
+    console.log(this.maxBilledByAge);
+    console.log(this.totalBilled);
+    this.percentage = ((this.maxBilledByAge / this.totalBilled) * 100);
   }
 
 }
