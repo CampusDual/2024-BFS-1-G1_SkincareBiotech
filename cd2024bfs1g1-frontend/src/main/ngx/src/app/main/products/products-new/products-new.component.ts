ba586336b3aedde065bca6a35e2900e66ae5ade2
iagopombo <iagopombo@gmail.com>
2024-06-07 10:52:16 +0200

    Added final price info to new product component

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products-new/products-new.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/products/products-new/products-new.component.ts
@@ -9,21 +9,7 @@ import { OCurrencyInputComponent, OSlideToggleComponent, OntimizeService } from 
 })
 export class ProductsNewComponent implements OnInit{
 
-  @ViewChild("proSaleToggle")
-  proSaleToggle: OSlideToggleComponent;
-
-  @ViewChild("proSaleCurrency")
-  proSaleCurrency: OCurrencyInputComponent;
-
-  @ViewChild("proPriceCurrency")
-  proPriceCurrency: OCurrencyInputComponent;
-
   service: OntimizeService;
-
-  commissionPlataform: number;
-  commissionRedSys: number;
-
-  public priceUser: number;
 
   constructor(
     private router: Router,
@@ -31,18 +17,7 @@ export class ProductsNewComponent implements OnInit{
   ) {
     this.service = this.injector.get(OntimizeService)
   }
-  ngOnInit(){
-    const conf = this.service.getDefaultServiceConfiguration('commissions');
-      this.service.configureService(conf);
-      this.service.query({}, ["COM_NAME","COM_VALUE"], "commission")
-        .subscribe((data) => {
-          if (data.data.length > 0) {
-            this.commissionRedSys = data.data.find((element) => (element.COM_NAME === "Redsys_commissions")).COM_VALUE;            
-            this.commissionPlataform = data.data.find((element) => (element.COM_NAME === "Plataform_commissions")).COM_VALUE;
-          }
-        })
-      this.priceUser = 0;
-  }
+  ngOnInit(){ }
   onInsert(success: boolean) {
     if (success) {
       this.router.navigate(['/main/products']);
@@ -50,22 +25,6 @@ export class ProductsNewComponent implements OnInit{
   }
 
   onChange(event) {
-    if (!this.proSaleToggle.isChecked()) {
-      this.proSaleCurrency.readOnly = true;
-      this.proSaleCurrency.setValue(null);
-      this.proPriceCurrency.readOnly = false;
-    } else {
-      this.proSaleCurrency.readOnly = false;
-      this.proSaleCurrency.enabled = this.proSaleToggle.getValue();     
-      this.proPriceCurrency.readOnly = true;
-    }
   }
 
-  onInputChanged(event){
-    if (!this.proSaleToggle.isChecked()) {
-      this.priceUser = (this.proPriceCurrency.getValue() / (1 - (this.commissionPlataform / 100))) / (1 - (this.commissionRedSys / 100));
-    } else {
-      this.priceUser = (this.proSaleCurrency.getValue() / (1 - (this.commissionPlataform / 100))) / (1 - (this.commissionRedSys / 100));
-    }
-  }
 }
