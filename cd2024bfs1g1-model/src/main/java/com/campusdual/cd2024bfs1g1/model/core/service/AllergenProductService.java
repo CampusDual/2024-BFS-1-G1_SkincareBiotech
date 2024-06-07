c9359a1d802d75e1494be49d376f2e8bbfb7c58f
BryanGrana <bryangranamartinez@gmail.com>
2024-06-07 13:52:43 +0200

    Errors fixed

--- cd2024bfs1g1-model/src/main/java/com/campusdual/cd2024bfs1g1/model/core/service/AllergenProductService.java
+++ cd2024bfs1g1-model/src/main/java/com/campusdual/cd2024bfs1g1/model/core/service/AllergenProductService.java
@@ -2,11 +2,15 @@ package com.campusdual.cd2024bfs1g1.model.core.service;
 
 import com.campusdual.cd2024bfs1g1.api.core.service.IAllergenProductService;
 import com.campusdual.cd2024bfs1g1.model.core.dao.AllergenProductDao;
+import com.campusdual.cd2024bfs1g1.model.core.utils.Utils;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import jdk.jshell.execution.Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,6 +33,14 @@ public class AllergenProductService implements IAllergenProductService {
 
     @Override
     public EntityResult allergenProductInsert(Map<String, Object> attributes) throws OntimizeJEERuntimeException {
-        return this.daoHelper.insert(this.allergenProductDao, attributes);
+        try {
+            return this.daoHelper.insert(this.allergenProductDao, attributes);
+
+        } catch (DuplicateKeyException e) {
+            EntityResult result = new EntityResultMapImpl();
+            result.setCode(EntityResult.OPERATION_WRONG);
+            result.setMessage("ALLERGEN_IS_DUPLICATED");
+            return result;
+        }
     }
 }
--- cd2024bfs1g1-model/src/main/resources/db/migration/V30__create_allergens_table.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V30__create_allergens_table.sql
@@ -6,14 +6,13 @@ CREATE TABLE public.allergens (
 );
 
 CREATE TABLE public.allergens_products (
+    ap_id serial not null,
 	pro_id int NOT NULL,
 	aller_id int NOT NULL,
+	CONSTRAINT allergens_products_pk PRIMARY KEY (ap_id),
 	CONSTRAINT allergens_products_products_fk_products FOREIGN KEY (pro_id) REFERENCES public.products(pro_id),
 	CONSTRAINT allergens_products_products_fk_allergens FOREIGN KEY (aller_id) REFERENCES public.allergens(aller_id)
 );
 
-ALTER TABLE public.allergens_products ADD ap_id serial NOT NULL;
-ALTER TABLE public.allergens_products ADD CONSTRAINT allergens_products_pk PRIMARY KEY (ap_id);
-
 INSERT INTO allergens (aller_name) VALUES('Amylcinnamaldehyde'), ('Benzyl Alcohol'), ('Benzyl Benzoate'), ('Benzyl Salicylate'), ('Cinnamyl Alcohol'), ('Cinnamal'), ('Citral'), ('Citronellol'), ('Coumarin'), ('Eugenol'), ('Farnesol'), ('Geraniol'), ('Hexyl Cinnamal'), ('Hydroxycitronellal'), ('Isoeugenol'), ('Limonene'), ('Linalool'), ('Butylphenyl Methylpropional (Lilial)'), ('Evernia Furfuracea Extract'), ('Evernia Prunastri Extract'), ('Hydroxyisohexyl 3-Cyclohexene Carboxaldehyde (Lyral)'), ('Methyl 2-Octynoate'), ('Alpha-Isomethyl Ionone'), ('Anisyl Alcohol'), ('Benzyl Cinnamate'), ('Citronellal'), ('Isoamyl Alcohol'), ('Isoamyl Acetate'), ('Cinnamyl Acetate'), ('Amylcinnamyl Alcohol'), ('Anisyl Acetate'), ('Benzyl Acetate'), ('Isoamyl Cinnamate'), ('Methyl Salicylate'), ('Methyl Eugenol');
-CREATE UNIQUE INDEX allergens_products_pro_id_idx ON public.allergens_products (pro_id,aller_id);
+ALTER TABLE public.allergens_products ADD CONSTRAINT allergens_products_unique UNIQUE (pro_id,aller_id);
