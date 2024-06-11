7158ce3c2368949ca231897c1112c993fa6e8b17
EduFergo <efergo93@gmail.com>
2024-06-11 12:50:07 +0200

    message added to billedAgeAndGender

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/billed-age/billed-age.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/billed-age/billed-age.component.ts
@@ -13,11 +13,8 @@ export class BilledAgeComponent {
   @ViewChild('ageAndGender')
   protected ageAndGender: OChartComponent
 
-  
-  data2: any[];
   chartParameters: PieChartConfiguration;
   service: OntimizeService;
-
 
   // Billed age
   minAge: number;
@@ -30,13 +27,16 @@ export class BilledAgeComponent {
   //Billed age&gender
   minAge2: number;
   maxAge2: number;
-  maxBilledByAgeAndGender: [string, number];
+  maxBilledGenre: string;
+  maxBilledGenreTotal: number;
+  totalBilled2: number;
   man: PersonData;
   woman: PersonData;
   pna: PersonData;
   other: PersonData;
   percentage2: number;
   isAgeGenderData: boolean;
+  genres = ['MAN', 'WOMAN', 'PNA', 'OTHER'];
 
   colorScheme = {
     domain: ['#31d4f8', '#2aaecb', '#1f6e9a', '#154865', '#0499ec', '#03649b', '#03649b']
@@ -48,105 +48,25 @@ export class BilledAgeComponent {
     this.chartParameters.legend.margin.top = 5;
     this.chartParameters.legend.margin.bottom = 5;
     this.chartParameters.legendPosition = 'right';
-    
+
   }
-  
+
 
   loadBilledAge(event: any) {
     this.initializeBilledAgeParams();
     this.groupDataByAgeRange(event, 'AGE_RANGE', 'TOTAL');
     this.calculateBilledAgeStatistics(event);
 
-    this.percentage = ((this.maxBilledByAge / this.totalBilled) * 100);
+    
   }
 
 
   loadAGeGender(event: any) {
-    const genres = ['MAN', 'WOMAN', 'PNA', 'OTHER']
-    this.minAge2 = 0;
-    this.maxAge2 = 0;
-    this.maxBilledByAgeAndGender = ['', 0];
 
-    
-    this.isAgeGenderData = false;
-
-    const groupedData = event.reduce((acc, item) => {
-      if (!acc[item.AGE_RANGE]) {
-        acc[item.AGE_RANGE] = {
-          MAN: 0,
-          WOMAN: 0,
-          PNA: 0,
-          OTHER: 0
-        };
-      }
-     
-      acc[item.AGE_RANGE]['WOMAN'] += item.WOMAN;
-      acc[item.AGE_RANGE]['PNA'] += item.PNA;
-      acc[item.AGE_RANGE]['OTHER'] += item.OTHER;
-      this.isAgeGenderData = true;
-      return acc;
-    }, {});
-
-    this.data2 = Object.keys(groupedData).map(key => {
-      return {
-        name: key,
-        series: [
-          {
-            name: "MAN",
-            value: groupedData[key].MAN
-          },
-          {
-            name: "WOMAN",
-            value: groupedData[key].WOMAN
-          },
-          {
-            name: "PNA",
-            value: groupedData[key].PNA
-          },
-          {
-            name: "OTHER",
-            value: groupedData[key].OTHER
-          }
-        ]
-      };
-    });
-
-    this.man = {
-      gender: "MAN",
-      total: 0,
-      minAge: 0,
-      maxAge: 0
-    };
-    this.woman = {
-      gender: "WOMAN",
-      total: 0,
-      minAge: 0,
-      maxAge: 0
-    };
-    this.other = {
-      gender: "OTHER",
-      total: 0,
-      minAge: 0,
-      maxAge: 0
-    };
-    this.pna = {
-      gender: "PNA",
-      total: 0,
-      minAge: 0,
-      maxAge: 0
-    };
-    for (let ageRange2 in event) {
-      console.log(event[ageRange2])
-      if (event[ageRange2].MAN > this.man) {
-        this.man = event[ageRange2].MAN;
-        this.minAge = event[ageRange2].GBA_MIN_AGE;
-        this.maxAge = event[ageRange2].GBA_MAX_AGE;
-
-      }
-      this.totalBilled += event[ageRange2].TOTAL;
-    }
-    console.log(this.maxBilledByAgeAndGender);
-    this.percentage2 = ((this.maxBilledByAgeAndGender[1] / this.totalBilled) * 100);
+    this.initializeBilledAgeGenderParams();
+    this.groupDataByAgeRangeAndGender(event, this.genres, 'AGE_RANGE');
+    this.calculateBilledAgeAndGenderStatistics(event);
+   
   }
 
   private initializeBilledAgeParams() {
@@ -157,13 +77,37 @@ export class BilledAgeComponent {
     this.isBilledAgeData = false;
   }
 
-  private groupDataByAgeRange(data: any[], ageRangeKey: string, valueKey: string){
-   return data.reduce((acc, item) => {
+  private initializeBilledAgeGenderParams() {
+    this.minAge2 = 0;
+    this.maxAge2 = 0;
+    this.maxBilledGenre = '';
+    this.isAgeGenderData = false;
+    this.maxBilledGenreTotal = 0;
+  }
+
+  private groupDataByAgeRange(data: any[], ageRangeKey: string, valueKey: string) {
+    return data.reduce((acc, item) => {
       if (!acc[item[ageRangeKey]]) {
         acc[item[ageRangeKey]] = 0;
       }
       acc[item[ageRangeKey]] += item[valueKey];
-      this.isAgeData = true;
+      this.isBilledAgeData = true;
+      return acc;
+    }, {});
+  }
+
+  private groupDataByAgeRangeAndGender(data: any[], genres: string[], ageRangeKey: string) {
+    return data.reduce((acc, item) => {
+      if (!acc[item[ageRangeKey]]) {
+        acc[item[ageRangeKey]] = {};
+        genres.forEach(genre => {
+          acc[item[ageRangeKey]][genre] = 0;
+        });
+      }
+      genres.forEach(genre => {
+        acc[item[ageRangeKey]][genre] += item[genre];
+      })
+      this.isAgeGenderData = true;
       return acc;
     }, {});
   }
@@ -177,8 +121,24 @@ export class BilledAgeComponent {
       }
       this.totalBilled += item.TOTAL;
     });
+    this.percentage = ((this.maxBilledByAge / this.totalBilled) * 100);
   }
-  
+
+  private calculateBilledAgeAndGenderStatistics(data:any[]){
+    let maxBilled = 0;
+      data.forEach(item => {
+      this.genres.forEach(genre => {
+        if (item[genre] > maxBilled) {
+          maxBilled = item[genre];
+          this.maxBilledGenreTotal = maxBilled;
+          this.maxBilledGenre = genre;
+          this.minAge2 = item['GBA_MIN_AGE'];
+          this.maxAge2 = item['GBA_MAX_AGE'];
+        }
+      });
+    });
+    this.percentage2 = ((this.maxBilledGenreTotal / this.totalBilled) * 100);
+  }
 }
 
 interface PersonData {
