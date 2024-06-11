903ea9309f48edad25fc020da05190624a08e193
Tania Vazquez Pose <tvaquezpose@gmail.com>
2024-06-11 14:10:27 +0200

    fix script V32 grupo6

--- cd2024bfs1g1-model/src/main/resources/db/migration/V32__create_skin_types_and_product_skin.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V32__create_skin_types_and_product_skin.sql
@@ -22,31 +22,33 @@ insert into skin_types (skin_name) values ('Normal');
 insert into skin_types (skin_name) values ('Mixta');
 
 UPDATE public.usr_role
-    SET rol_json_client_permission='{ "menu": [ { "attr": "products", "visible": false, "enabled": false },
+    SET rol_json_client_permission='{ "menu": [
+    { "attr": "products", "visible": false, "enabled": false },
     { "attr": "orders", "visible": false, "enabled": false },
     { "attr": "brands", "visible": false, "enabled": false },
     { "attr": "categories", "visible": false, "enabled": false },
     { "attr": "home", "visible": false, "enabled": false },
     { "attr": "login", "visible": false, "enabled": false },
     { "attr": "logout", "visible": false, "enabled": false },
-    {"attr": "data-analysis", "visible": false, "enabled": false },
-    {"attr": "skin-types", "visible": false, "enabled": false },
+    { "attr": "data-analysis", "visible": false, "enabled": false },
+    { "attr": "skin-types", "visible": false, "enabled": false },
     { "attr": "sellers", "visible": false, "enabled": false },
-    {"attr": "sells-by-category", "visible": false, "enabled": false },
-    {"attr": "billed-age", "visible": false, "enabled": false },
-    { "attr": "allergens", "visible": false, "enabled": false }]}'
+    { "attr": "sells-by-category", "visible": false, "enabled": false },
+    { "attr": "allergens", "visible": false, "enabled": false },
+    { "attr": "billed-age", "visible": false, "enabled": false }]}'
     WHERE rol_name='user';
 UPDATE public.usr_role
-    SET rol_json_client_permission='{ "menu": [ { "attr": "productsPublic", "visible": false, "enabled": false },
+    SET rol_json_client_permission='{ "menu": [
+     { "attr": "productsPublic", "visible": false, "enabled": false },
      { "attr": "featuredPublic", "visible": false, "enabled": false },
      { "attr": "history", "visible": false, "enabled": false },
      { "attr": "login", "visible": false, "enabled": false },
      { "attr": "cart", "visible": false, "enabled": false },
-     {"attr": "skin-types", "visible": false, "enabled": false },
+     { "attr": "skin-types", "visible": false, "enabled": false },
      { "attr": "billed-age", "visible": false, "enabled": false },
      { "attr": "sellers", "visible": false, "enabled": false },
      { "attr": "profile", "visible": false, "enabled": false },
-     { "attr": "sells-by-category", "visible": false, "enabled": false },
-     { "attr": "allergens", "visible": false, "enabled": false } ] }'
+     { "attr": "allergens", "visible": false, "enabled": false },
+     { "attr": "sells-by-category", "visible": false, "enabled": false } ] }'
     WHERE rol_name='seller';
 
