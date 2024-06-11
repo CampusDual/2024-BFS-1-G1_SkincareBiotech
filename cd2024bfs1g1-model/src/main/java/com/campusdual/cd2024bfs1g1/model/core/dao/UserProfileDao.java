e266cfa0bf3bdf583cbf5558bda0db54cfb4fce3
Martin Amor <martin.amor2004@gmail.com>
2024-06-11 12:14:42 +0200

    Delete check column

--- cd2024bfs1g1-model/src/main/java/com/campusdual/cd2024bfs1g1/model/core/dao/UserProfileDao.java
+++ cd2024bfs1g1-model/src/main/java/com/campusdual/cd2024bfs1g1/model/core/dao/UserProfileDao.java
@@ -18,6 +18,5 @@ public class UserProfileDao extends OntimizeJdbcDaoSupport {
     public static final String USR_ID = "USR_ID";
     public static final String UGE_ID = "UGE_ID";
     public static final String UPR_ZIPCODE = "UPR_ZIPCODE";
-    public static final String UPR_CHECK = "UPR_CHECK";
 
 }
