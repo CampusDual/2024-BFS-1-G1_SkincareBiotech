39e0350267ca36aa1c7c64b082824646a477db33
Sergio Méndez Losada <sergio.mendez@campusdual.com>
2024-06-12 09:10:34 +0200

    Fixed V32

--- cd2024bfs1g1-model/src/main/resources/db/migration/V32__alter_usr_profile2_table.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V32__alter_usr_profile2_table.sql
@@ -1,3 +1,5 @@
 ALTER TABLE public.usr_profile ADD COLUMN upr_zipcode varchar;
 
 ALTER TABLE public.usr_profile ALTER COLUMN upr_address DROP NOT NULL;
+
+ALTER TABLE public.orders ALTER COLUMN ord_phone TYPE varchar USING ord_phone::varchar;
