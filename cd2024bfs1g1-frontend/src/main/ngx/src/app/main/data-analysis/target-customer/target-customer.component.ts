de80a386739b1b1eb43d013a244470e45ec7d283
Susana Roca <sroca754@gmail.com>
2024-06-12 13:13:50 +0200

    added component

---
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/main/data-analysis/target-customer/target-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-target-customer',
+  templateUrl: './target-customer.component.html',
+  styleUrls: ['./target-customer.component.css']
+})
+export class TargetCustomerComponent {
+
+}
