a269cf8bba9730f6ece8ad48ac067b5ec6a49c28
Susana Roca <sroca754@gmail.com>
2024-06-12 08:36:35 +0200

    updated flayway files

--- cd2024bfs1g1-model/src/main/resources/db/migration/V35__update_permissions.sql
+++ cd2024bfs1g1-model/src/main/resources/db/migration/V35__update_permissions.sql
@@ -13,6 +13,7 @@ UPDATE public.usr_role
     { "attr": "sells-by-category", "visible": false, "enabled": false },
     { "attr": "allergens", "visible": false, "enabled": false },
     { "attr": "billed-age", "visible": false, "enabled": false },
+    { "attr": "users-skin-types", "visible": false, "enabled": false },
     { "attr": "customer-analysis", "visible": false, "enabled": false},
     { "attr": "age-range-configuration", "visible": false, "enabled": false}]}'
     WHERE rol_name='user';
@@ -29,6 +30,7 @@ UPDATE public.usr_role
      { "attr": "profile", "visible": false, "enabled": false },
      { "attr": "allergens", "visible": false, "enabled": false },
      { "attr": "sells-by-category", "visible": false, "enabled": false },
+     { "attr": "users-skin-types", "visible": false, "enabled": false },
      { "attr": "customer-analysis", "visible": false, "enabled": false },
      { "attr": "age-range-configuration", "visible": false, "enabled": false}]}'
     WHERE rol_name='seller';
