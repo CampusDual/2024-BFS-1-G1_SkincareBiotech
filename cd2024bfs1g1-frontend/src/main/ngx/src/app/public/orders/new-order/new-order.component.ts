8daffbef0f7ddba0602faeeaf87ac9cf50da7c79
Martin Amor <martin.amor2004@gmail.com>
2024-06-14 14:29:27 +0200

    Reorganiced code

--- cd2024bfs1g1-frontend/src/main/ngx/src/app/public/orders/new-order/new-order.component.ts
+++ cd2024bfs1g1-frontend/src/main/ngx/src/app/public/orders/new-order/new-order.component.ts
@@ -68,7 +68,7 @@ export class NewOrderComponent implements AfterViewInit, OnInit {
     this.translate = this.injector.get(OTranslateService);
     this.validatorZip.push(Validators.minLength(5));
     this.validatorZip.push(Validators.maxLength(5));
-    this.validatorZip.push(Validators.pattern('^[0-9]*$')); 
+    this.validatorZip.push(Validators.pattern('^[0-9]*$'));
     this.validatorPhone.push(Validators.minLength(9));
     this.validatorPhone.push(Validators.maxLength(9));
     this.validatorPhone.push(Validators.pattern('^[6-9][0-9]*$'));
@@ -124,7 +124,7 @@ export class NewOrderComponent implements AfterViewInit, OnInit {
       this.defAddress.checked = false;
     }
 
-    
+
 
   }
 
@@ -154,47 +154,47 @@ export class NewOrderComponent implements AfterViewInit, OnInit {
   }
   submitOrder(): void {
 
-    if (this.authService.isLoggedIn()) {
-
-      const conf = this.service.getDefaultServiceConfiguration('orders');
-      this.service.configureService(conf);
-      let data = {
-        ORD_NAME: this.nameInput.getValue(),
-        ORD_PHONE: this.phoneInput.getValue(),
-        ORD_ZIPCODE: this.zipInput.getValue(),
-        ORD_ADDRESS: this.addressInput.getValue(),
-        ORD_ITEMS: this.cartService.getCart(),
-        UPR_ADDRESS: this.addressInput.getValue(),
-        USR_PHONE: this.phoneInput.getValue(),
-        USR_ZIP: this.zipInput.getValue(),
-        USR_ID: this.usrId.getValue(),
-        UPR_RECIPIENT: this.nameInput.getValue(),
-      }
-      if (data.ORD_NAME != null && data.ORD_PHONE != null && data.ORD_ZIPCODE != null && data.ORD_ADDRESS != null 
-        && data.USR_ZIP.length==5 && data.USR_PHONE.length==9 && /^[6789]\d{8}$/.test(data.USR_PHONE) 
-        && data.UPR_ADDRESS.length>1  && data.UPR_RECIPIENT.length>1 && /^\d{5}$/.test(data.USR_ZIP)) {
-        this.cartService.emptyCart();
-        this.service.insert(data, "order").subscribe(res => {
-          this.order = (res.data["ORD_ID"]).toString().padStart(12, "0");
-          this.orderView = (res.data["ORD_ID"]).toString();
-          this.price = (this.totalAmount * 100).toFixed(0);
-          this.submitRedsysOrder();
-        })
-        if (this.defAddress.checked==true){
-          this.updateProfile(data)
-        }
-      } else {
-        this.dialogService.alert('Error', this.translate.get(
-          'DATA_FORM_INVALID'
-          ));
-
-      }
-
-    } else {
+    if (!this.authService.isLoggedIn()) {
       this.router.navigate(['/login'],
         { queryParams: { 'session-not-started': 'true' } }
-      )  // En el futuro si se cambia esta clase mantener la redireccion de este caso de uso
+      );  // En el futuro si se cambia esta clase mantener la redireccion de este caso de uso
+      return;
     }
+
+    const conf = this.service.getDefaultServiceConfiguration('orders');
+    this.service.configureService(conf);
+    let data = {
+      ORD_NAME: this.nameInput.getValue(),
+      ORD_PHONE: this.phoneInput.getValue(),
+      ORD_ZIPCODE: this.zipInput.getValue(),
+      ORD_ADDRESS: this.addressInput.getValue(),
+      ORD_ITEMS: this.cartService.getCart(),
+      UPR_ADDRESS: this.addressInput.getValue(),
+      USR_PHONE: this.phoneInput.getValue(),
+      USR_ZIP: this.zipInput.getValue(),
+      USR_ID: this.usrId.getValue(),
+      UPR_RECIPIENT: this.nameInput.getValue(),
+    }
+    if (!(data.ORD_NAME != null && data.ORD_PHONE != null && data.ORD_ZIPCODE != null && data.ORD_ADDRESS != null
+      && data.USR_ZIP.length == 5 && data.USR_PHONE.length == 9 && /^[6789]\d{8}$/.test(data.USR_PHONE)
+      && data.UPR_ADDRESS.length > 1 && data.UPR_RECIPIENT.length > 1 && /^\d{5}$/.test(data.USR_ZIP))) {
+      this.dialogService.alert('Error', this.translate.get(
+        'DATA_FORM_INVALID'
+      ));
+      return;
+    }
+    this.cartService.emptyCart();
+    this.service.insert(data, "order").subscribe(res => {
+      this.order = (res.data["ORD_ID"]).toString().padStart(12, "0");
+      this.orderView = (res.data["ORD_ID"]).toString();
+      this.price = (this.totalAmount * 100).toFixed(0);
+      this.submitRedsysOrder();
+    })
+    if (this.defAddress.checked == true) {
+      this.updateProfile(data)
+    }
+
+
 
   }
   currentLang(): void {
@@ -216,59 +216,59 @@ export class NewOrderComponent implements AfterViewInit, OnInit {
   }
   submitRedsysOrder(): void {
 
-      this.url = document.querySelector("base").href;
-      this.currentLang();
-      // Datos de la transacción
-      const datosTransaccion = {
-        "DS_MERCHANT_AMOUNT": this.price,//this.price, // Los dos últimos son decimales (5000 = 50,00)
-        "DS_MERCHANT_CURRENCY": "978",
-        "DS_MERCHANT_MERCHANTCODE": "999008881",
-        "DS_MERCHANT_MERCHANTURL": this.url,
-        "DS_MERCHANT_ORDER": this.order,//this.order, // No se puede repetir(= id pedido)
-        "DS_MERCHANT_TERMINAL": "1",
-        "DS_MERCHANT_TRANSACTIONTYPE": "0",
-        "DS_MERCHANT_URLKO": `${this.url}/order/rejected/${this.order}`,
-        "DS_MERCHANT_URLOK": `${this.url}/profile/${this.orderView}`,
-        "DS_MERCHANT_CONSUMERLANGUAGE": this.currLang // 1: Español - 2:Inglés
-      }
+    this.url = document.querySelector("base").href;
+    this.currentLang();
+    // Datos de la transacción
+    const datosTransaccion = {
+      "DS_MERCHANT_AMOUNT": this.price,//this.price, // Los dos últimos son decimales (5000 = 50,00)
+      "DS_MERCHANT_CURRENCY": "978",
+      "DS_MERCHANT_MERCHANTCODE": "999008881",
+      "DS_MERCHANT_MERCHANTURL": this.url,
+      "DS_MERCHANT_ORDER": this.order,//this.order, // No se puede repetir(= id pedido)
+      "DS_MERCHANT_TERMINAL": "1",
+      "DS_MERCHANT_TRANSACTIONTYPE": "0",
+      "DS_MERCHANT_URLKO": `${this.url}/order/rejected/${this.order}`,
+      "DS_MERCHANT_URLOK": `${this.url}/profile/${this.orderView}`,
+      "DS_MERCHANT_CONSUMERLANGUAGE": this.currLang // 1: Español - 2:Inglés
+    }
 
-      // 1 - Decodificar la clave del comercio en BASE64
-      const claveComercio = "sq7HjrUOBfKmC576ILgskD5srU870gJ7";
-      const claveComercioWordArray = CryptoJS.enc.Base64.parse(claveComercio);
+    // 1 - Decodificar la clave del comercio en BASE64
+    const claveComercio = "sq7HjrUOBfKmC576ILgskD5srU870gJ7";
+    const claveComercioWordArray = CryptoJS.enc.Base64.parse(claveComercio);
 
-      // 2 - Diversificar la clave de firma realizando un cifrado 3DES
+    // 2 - Diversificar la clave de firma realizando un cifrado 3DES
 
-      // Se define el vector de inicialización (IV)
-      const iv = CryptoJS.enc.Hex.parse("0000000000000000");
+    // Se define el vector de inicialización (IV)
+    const iv = CryptoJS.enc.Hex.parse("0000000000000000");
 
-      // Se realiza el cifrado 3DES del número de pedido de la transacción
-      const cifrado = CryptoJS.TripleDES.encrypt(datosTransaccion.DS_MERCHANT_ORDER, claveComercioWordArray, {
-        iv: iv, // Se utiliza el vector de inicialización definido anteriormente
-        mode: CryptoJS.mode.CBC, // Se utiliza el modo de operación Cipher Block Chaining (CBC)
-        padding: CryptoJS.pad.ZeroPadding // No se aplica ningún tipo de relleno (padding)
-      });
+    // Se realiza el cifrado 3DES del número de pedido de la transacción
+    const cifrado = CryptoJS.TripleDES.encrypt(datosTransaccion.DS_MERCHANT_ORDER, claveComercioWordArray, {
+      iv: iv, // Se utiliza el vector de inicialización definido anteriormente
+      mode: CryptoJS.mode.CBC, // Se utiliza el modo de operación Cipher Block Chaining (CBC)
+      padding: CryptoJS.pad.ZeroPadding // No se aplica ningún tipo de relleno (padding)
+    });
 
-      // 3 - Codificar los datos de la transacción en Base64 (principio documento oficial)
-      const datosTransaccionWordArray = CryptoJS.enc.Utf8.parse(JSON.stringify(datosTransaccion));
-      const datosTransaccionBase64 = CryptoJS.enc.Base64.stringify(datosTransaccionWordArray);
+    // 3 - Codificar los datos de la transacción en Base64 (principio documento oficial)
+    const datosTransaccionWordArray = CryptoJS.enc.Utf8.parse(JSON.stringify(datosTransaccion));
+    const datosTransaccionBase64 = CryptoJS.enc.Base64.stringify(datosTransaccionWordArray);
 
-      // 4 - Calcular el HMAC-256 con los datos de la transacción y la clave diversificada
-      const firma = CryptoJS.HmacSHA256(datosTransaccionBase64, cifrado.ciphertext);
+    // 4 - Calcular el HMAC-256 con los datos de la transacción y la clave diversificada
+    const firma = CryptoJS.HmacSHA256(datosTransaccionBase64, cifrado.ciphertext);
 
-      // 5 - Codificar la firma en Base64
-      const firmaBase64 = CryptoJS.enc.Base64.stringify(firma);
+    // 5 - Codificar la firma en Base64
+    const firmaBase64 = CryptoJS.enc.Base64.stringify(firma);
 
-      // Establecer el valor de los campos en el formulario HTML
+    // Establecer el valor de los campos en el formulario HTML
 
-      this.ds_merchantParameters.nativeElement.value = datosTransaccionBase64;
-      this.ds_signature.nativeElement.value = firmaBase64;
-      //console.log(this.ds_merchantParameters.nativeElement.value);
+    this.ds_merchantParameters.nativeElement.value = datosTransaccionBase64;
+    this.ds_signature.nativeElement.value = firmaBase64;
+    //console.log(this.ds_merchantParameters.nativeElement.value);
 
-      // Enviar el formulario después de establecer los valores
-      const form = document.forms['form'];
-      if (form) {
-        form.submit();
-      }
+    // Enviar el formulario después de establecer los valores
+    const form = document.forms['form'];
+    if (form) {
+      form.submit();
     }
   }
+}
 
